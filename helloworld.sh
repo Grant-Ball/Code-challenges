e0d629b8aba0645b50fa7fbdac074c4e1a0d7dde
grant <gwball99@gmail.com>
2022-11-09 21:52:11 +0000

    helloworld.sh

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Grant Ball
+# Date of latest revision:11-9-22
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+A="I am working perfectly"
+echo $A
+
+# End
