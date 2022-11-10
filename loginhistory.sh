d3871c0c752b818480975568b76467a0392ca336
grant <gwball99@gmail.com>
2022-11-10 01:54:39 +0000

    .

---
+++ loginhistory.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script:login history                       
+# Author:Grant Ball                       
+# Date of latest revision:11-9-22      
+# Purpose:view login history      
+
+# Basic function
+
+A="last"
+
+login_history () {
+    $A
+}
+
+login_history
+login_history
