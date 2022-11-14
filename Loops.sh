4ca44b60972411d035972066ef69dc6800eaf03d
grant <gwball99@gmail.com>
2022-11-14 03:36:02 +0000

    couldnt figure out how to do challenge

---
+++ Loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script:Loops                       
+# Author:Grant Ball                       
+# Date of latest revision:11-13-22      
+# Purpose:Practice creating loops.                      
+
+# Basic for loop
+names="Stan Kyle Cartman"
+for name in $names
+do
+  echo $name
+done
+
+echo "Script complete"
+
+# End
