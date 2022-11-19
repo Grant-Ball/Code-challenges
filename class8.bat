2720270c4700885b27a5322a3ebf8276bd7fe6bf
Grant-Ball <gwball99@gmail.com>
2022-11-19 15:25:04 -0800

    my first batch script

---
+++ class8.bat
@@ -0,0 +1,6 @@
+:: Script: Batch command
+:: Author: Grant Ball
+:: Last Date Revised: 11-19-22
+:: Purpose: To learn how to write Batch commands for the first time.
+
+ROBOCOPY /s "C:\Users\j.thompson\Desktop\My Project Files" "C:\Users\j.thompson\Desktop\copyfolder" /LOG:backuplog.txt
