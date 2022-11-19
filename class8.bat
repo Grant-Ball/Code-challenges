1b28847de105abb74e09d26985be61a891354a30
Grant-Ball <gwball99@gmail.com>
2022-11-19 15:34:35 -0800

    small change

--- class8.bat
+++ class8.bat
@@ -3,4 +3,7 @@
 :: Last Date Revised: 11-19-22
 :: Purpose: To learn how to write Batch commands for the first time.
 
+:: This command will copy the contents of Jorges "my project files" folder on his desktop to his "copyfolder" on his desktop also
+:: This is scheduled to happen every day at midnight
+
 ROBOCOPY /s "C:\Users\j.thompson\Desktop\My Project Files" "C:\Users\j.thompson\Desktop\copyfolder" /LOG:backuplog.txt
