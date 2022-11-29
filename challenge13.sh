86cc41526becf97ed4271691e204ae727ed0c89c
Grant Ball <gwball99@gmail.com>
2022-11-29 09:21:45 -0700

    I finally got my code to work properly and am ready for a nap

--- challenge13.sh
+++ challenge13.sh
@@ -3,12 +3,39 @@
 # Date of latest revision:11-27-22      
 # Purpose:To learn and practice whois
 
-function google
+# I forgot to ACP before I made a SSH connection into my lab computer because this wasn't working on my computer so it got deleted.
+# It workds now and I had to rewrite it and I didnt want to yesterday (11-28-22) so I rewrote it this morning (11-29-22)
+
+F=domain_info.txt
+
+echo "Enter domain you want to get info about"
+read domain
+
+touch $F
+
+function step1
 {
-    G=google.com
-    touch ./single.txt
-    whois $G | tee single.txt
-    dig $G | tee single.txt
-    host $G | tee single.txt
-    nslookup $G | tee single.txt
+    whois $domain>> $F
 }
+
+function step2
+{
+    dig $domain>> $F
+}
+
+function step3
+{
+    host $domain>> $F
+}
+
+function step4
+{
+    nslookup $domain>> $F
+}
+
+step1
+step2
+step3
+step4
+
+nano $F
